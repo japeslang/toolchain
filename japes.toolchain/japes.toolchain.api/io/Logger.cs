f558fcb6f50fbed18cef16934e688c2d8b138475
thylordroot <thylordroot@gmail.com>
2026-07-02 03:26:18 -0500

    This is a port from the Xilang project, whichI found collided with another established programming language (https://www.cs.cornell.edu/courses/cs4120/2011fa/handouts/language.pdf). I wanted to rewrite things anyway, so I decided to merge efforts here. As a result, some of the headers got janked up. That has been corrected.

--- japes.toolchain/japes.toolchain.api/io/Logger.cs
+++ japes.toolchain/japes.toolchain.api/io/Logger.cs
@@ -1,18 +1,18 @@
 ﻿/*
- This file is part of the Xi Programming Language.
+ This file is part of the Japes Programming Language.
 
- Xi is free software: you can redistribute it and/or modify
+ Japes is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as
  published by the Free Software Foundation, either version 3 of
  the License, or (at your option) any later version.
 
- Xi is distributed in the hope that it will be useful,
+ Japes is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU Lesser General Public
- License along with Xi.  If not, see <http://www.gnu.org/licenses/>.
+ License along with Japes.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /* WARNING: ALL MEMBERS IN THIS API ARE SUBJECT TO MAJOR REVISION UNTIL BOOTSTRAPPED. */
